cf8ba1bc8ebc75e0173b66396f424bee7f2e2e41
Saevrose <C0859540@mylambton.ca>
2022-12-07 18:10:33 -0500

    Changing Neurons to 400

--- PongGame.py
+++ PongGame.py
@@ -1,11 +1,12 @@
 """ Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym. """
+'''adding comment to ensure git commit/push are working correctly'''
 import numpy as np
 import pickle as pickle
 import gym
 
 
 # hyperparameters
-H = 200 # number of hidden layer neurons
+H = 400 # number of hidden layer neurons
 batch_size = 10 # every how many episodes to do a param update?
 learning_rate = 1e-4
 gamma = 0.99 # discount factor for reward
